e0d4d807e3a0069d4f71b104f525be6d2f111afd
vinnyvilasboa <vinnycesca@gmail.com>
2022-05-01 00:10:18 -0400

    API: working

---
+++ flex-project/app.js
@@ -0,0 +1,22 @@
+let weather = {
+    apiKey : "772a5938a64f5d0f8aa3e42401341db0",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid=" 
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        console.log(name,icon,description, temp)
+        document.querySelector(".temp-city").innerHTML = "The Weather in " + name + " is:";
+        document.querySelector(".temp-degree").innerHTML = temp;
+        document.querySelector(".temp-description").innerHTML = description;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    }
+}
