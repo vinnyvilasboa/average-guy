f5e1863c5447ebb8d4df2c51ed2bc0b89b4c4b92
vinnyvilasboa <vinnycesca@gmail.com>
2022-05-01 00:44:11 -0400

    API: weather api working on button/enter

--- flex-project/app.js
+++ flex-project/app.js
@@ -1,22 +1,40 @@
 let weather = {
-    apiKey : "772a5938a64f5d0f8aa3e42401341db0",
-    fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=imperial&appid=" 
-        + this.apiKey
+    apiKey: "772a5938a64f5d0f8aa3e42401341db0",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=imperial&appid="
+            + this.apiKey
         )
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data))
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
     },
-    displayWeather: function (data){
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        console.log(name,icon,description, temp)
-        document.querySelector(".temp-city").innerHTML = "The Weather in " + name + " is:";
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        console.log(name, icon, description, temp)
+        document.querySelector(".temp-city").innerHTML = "Weather in " + name + " is:";
         document.querySelector(".temp-degree").innerHTML = temp;
         document.querySelector(".temp-description").innerHTML = description;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 }
+
+
+////this button below will have to have all apis passed into it. 
+
+document.querySelector('.search button').addEventListener("click", function () {
+    weather.search();
+})
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
